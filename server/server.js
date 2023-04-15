7e50d4832506285ee7d921ee4375bbc16b3a8d93
dannymsun <82359154+dannymsun@users.noreply.github.com>
2023-04-15 16:40:55 -0400

    reformatted path params

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ app.use(cors({
 
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
-app.get('/home/:redistricting_id:type:year', routes.home);
-app.get('/analytics/:redistricting_id:type:year', routes.analytics);
-app.get('/comparison/:redistricting_id_1:redistricting_id_2', routes.comparison);
+app.get('/home/:redistricting_id/:type/:year', routes.home);
+app.get('/analytics/:redistricting_id/:type/:year', routes.analytics);
+app.get('/comparison/:redistricting_id_1/:redistricting_id_2', routes.comparison);
 app.get('/create/:state', routes.create);
 
 app.listen(config.server_port, () => {
