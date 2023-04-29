aec9efbcea6bf628f523f50b15d5e6b71a7bfc26
dannymsun <82359154+dannymsun@users.noreply.github.com>
2023-04-28 23:16:13 -0400

    added states

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -7,12 +7,14 @@ const config = require('../config.json');
 export default function CreatePage() {
 
   const [state, setState] = useState('');
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch(`http://${config.server_host}:${config.server_port}/create/${state}`)
       .then(res => res.json())
-      .then(resJson => setAlbums(resJson));
+      .then(resJson => setData(resJson));
   }, []);
 
+  
 
 }
