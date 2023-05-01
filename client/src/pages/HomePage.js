8ac76a54ad506fc3dc68d2e1b397ebcfb9b618b7
anvitrao <76935670+anvitrao@users.noreply.github.com>
2023-05-01 17:07:50 -0400

    home page front end files

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,90 +1,240 @@
-import { useEffect, useState } from 'react';
-import { Container, Divider, Link } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams, useLocation} from 'react-router-dom';
+import { Button, Checkbox, Container, FormControlLabel, Grid, Link, Slider, TextField, makeStyles} from '@mui/material';
+import { Dropdown } from 'react-dropdown-now';
+import { DataGrid, gridColumnsTotalWidthSelector } from '@mui/x-data-grid';
+import './HomePage.css';
 
-import LazyTable from '../components/LazyTable';
-import SongCard from '../components/SongCard';
 const config = require('../config.json');
 
 export default function HomePage() {
-  // We use the setState hook to persist information across renders (such as the result of our API calls)
-  const [songOfTheDay, setSongOfTheDay] = useState({});
-  // TODO (TASK 13): add a state variable to store the app author (default to '')
-  const [author, setAuthor] = useState('');
 
-  const [selectedSongId, setSelectedSongId] = useState(null);
+  
+  const {search} = useLocation()  
+  const queryParams = new URLSearchParams(search)  
+  const [states, setStates] = useState([]) 
+  const [districts, setDistricts] = useState([])
+  const [districtings, setDistrictings] = useState([])
+  const [state, setState] = useState(queryParams.get('state'))  
+  const [district, setDistrict] = useState(queryParams.get('district')) 
+  const [year, setYear]  = useState(queryParams.get('year')) 
+  const [redistricting_id, setRedistrictingId] = useState(queryParams.get('redistricting')) 
+  const [election_type, setElectionType] = useState(queryParams.get('election_type')) 
 
-  // The useEffect hook by default runs the provided callback after every render
-  // The second (optional) argument, [], is the dependency array which signals
-  // to the hook to only run the provided callback if the value of the dependency array
-  // changes from the previous render. In this case, an empty array means the callback
-  // will only run on the very first render.
+  // const [year, setYear] = useState(2016);   
+  // const [redistricting_id, setRedistrictingId] = useState('Default'); 
+  // const [election_type, setElectionType] = useState('presidential'); 
+  const [data, setData] = useState([]);
+  const [summary, setSummary] = useState([]); 
+  const [summary_text, setSummaryText]= useState(``); 
+  const [result, setResult] = useState('')
+  console.log(`State: ${state}`) 
+  console.log(`District: ${district}`) 
+  console.log(`Election Type: ${election_type}`)
+   
+
   useEffect(() => {
-    // Fetch request to get the song of the day. Fetch runs asynchronously.
-    // The .then() method is called when the fetch request is complete
-    // and proceeds to convert the result to a JSON which is finally placed in state.
-    fetch(`http://${config.server_host}:${config.server_port}/random`)
-      .then(res => res.json())
-      .then(resJson => setSongOfTheDay(resJson));
-
-    // TODO (TASK 14): add a fetch call to get the app author (name not pennkey) and store it in the state variable
-    fetch(`http://${config.server_host}:${config.server_port}/author/name`)
-      .then(res => res.text())
-      .then(resText=> setAuthor(resText));
+    fetch(`http://${config.server_host}:${config.server_port}/get_states`)
+      .then(res => {return res.json()})
+      .then(resJson => { 
+        const states = resJson.map((outcome) => (outcome.state));
+        states.push('All')
+        setStates(states);
+      })
+      .catch(err => console.log(err));
   }, []);
 
-  // Here, we define the columns of the "Top Songs" table. The songColumns variable is an array (in order)
-  // of objects with each object representing a column. Each object has a "field" property representing
-  // what data field to display from the raw data, "headerName" property representing the column label,
-  // and an optional renderCell property which given a row returns a custom JSX element to display in the cell.
-  const songColumns = [
-    {
-      field: 'title',
-      headerName: 'Song Title',
-      renderCell: (row) => <Link onClick={() => setSelectedSongId(row.song_id)}>{row.title}</Link> // A Link component is used just for formatting purposes
-    },
-    {
-      field: 'album',
-      headerName: 'Album',
-      renderCell: (row) => <NavLink to={`/albums/${row.album_id}`}>{row.album}</NavLink> // A NavLink component is used to create a link to the album page
-    },
-    {
-      field: 'plays',
-      headerName: 'Plays'
-    },
+  useEffect(() => {
+    fetch(`http://${config.server_host}:${config.server_port}/get_districtings`)
+    .then(res => {return res.json()})
+    .then(resJson => {
+      const districtings = resJson.map((outcome) => (outcome.name));
+      setDistrictings(districtings);
+    })
+    .catch(err => console.log(err));
+  }, [state, year, election_type]);
+
+  useEffect(() => {
+    if (state !== 'All' && state !== null) {
+      fetch(`http://${config.server_host}:${config.server_port}/get_districts/?state=${state}`)
+      .then(res => {return res.json()})
+      .then(resJson => {
+        const districts = resJson.map((outcome) => (outcome.num_districts));
+        const array = Array.from({ length: districts }, (_, index) => index + 1)
+        array.push('All')
+        setDistricts(array);
+        
+      })
+      .catch(err => console.log(err));
+    } else {
+      setDistricts([])
+    }
+  }, [state]);
+
+  
+  useEffect(() => {
+    let url = `http://${config.server_host}:${config.server_port}/home/?redistricting_id=${redistricting_id}&election_type=${election_type}&year=${year}`
+    if (state !== 'All' && state !== null) {
+      url += `&state=${state}`;
+      if (district !== 'All' && district !== null) {
+        url += `&district=${district}`; 
+      }
+    } 
+  console.log(`Fetch URL: ${url}`);
+    fetch(url)
+      .then(res => {return res.json()})
+      .then(resJson => {     
+        console.log(resJson['election_data']) 
+        console.log(resJson['election_summary'])
+        const votes = resJson['election_data'].map((outcome) => ({id: outcome.state + outcome.county + outcome.precinct + outcome.district, ... outcome}));
+        setData(votes); 
+        setSummary(resJson['election_summary'])
+      })
+      .catch(err => console.log(err));
+  }, [redistricting_id, election_type, year,
+  state, district]);
+
+  useEffect(() => {
+    if ((district != 'All' && district != null) || ((election_type == 'presidential') && (state != 'All') && (state != null))) {
+      let tempSummaryText = ``;
+      for (let i = 0; i < summary.length; i++) { 
+        console.log('line 60')
+        console.log(summary[i])
+        const result = `<strong>${summary[i]['party']}</strong>: ${summary[i]['num_votes'].toLocaleString()} Votes `;
+        tempSummaryText += result
+      } 
+      setSummaryText(tempSummaryText);
+    } else {
+      if (election_type == 'presidential') {
+        console.log(summary)
+        let tempSummaryText = ``;
+        for (let i = 0; i < summary.length; i++) {  
+          console.log('line 69') 
+          const result = `<strong>${summary[i]['party']}</strong>: ${summary[i]['electoral_votes'].toLocaleString()} Electoral Votes `;
+          tempSummaryText += result;
+        }
+        setSummaryText(tempSummaryText)
+      } else {
+        console.log('line 67')
+        let tempSummaryText = ``;
+        for (let i = 0; i < summary.length; i++) { 
+          const result = `<strong>${summary[i]['party']}</strong>: ${summary[i]['num_seats'].toLocaleString()} Seats `;
+          tempSummaryText = tempSummaryText + result
+        }
+        setSummaryText(tempSummaryText)
+      }
+    }
+  }, [summary]);
+
+  const columns = [
+    { field: 'state', headerName: 'State'},
+    { field: 'county', headerName: 'County' },
+    { field: 'precinct', headerName: 'Precinct' },
+    { field: 'district', headerName: 'District' },
+    { field: 'republican_votes', headerName: 'Republican' },
+    { field: 'democratic_votes', headerName: 'Democrat' },
+    { field: 'independent_votes', headerName: 'Independent' },
+    { field: 'green_votes', headerName: 'Green Votes' },
+    { field: 'libertarian_votes', headerName: 'Libertarian' },
+    { field: 'constitution_votes', headerName: 'Constitution' },
   ];
 
-  // TODO (TASK 15): define the columns for the top albums (schema is Album Title, Plays), where Album Title is a link to the album page
-  // Hint: this should be very similar to songColumns defined above, but has 2 columns instead of 3
-  const albumColumns = [
-    {
-      field: 'album',
-      headerName: 'Album Title',
-      renderCell: (row) => <NavLink to={`/albums/${row.album_id}`}>{row.title}</NavLink> // A NavLink component is used to create a link to the album page
-    },
-    {
-      field: 'plays',
-      headerName: 'Plays'
-    },
-  ]
 
   return (
     <Container>
-      {/* SongCard is a custom component that we made. selectedSongId && <SongCard .../> makes use of short-circuit logic to only render the SongCard if a non-null song is selected */}
-      {selectedSongId && <SongCard songId={selectedSongId} handleClose={() => setSelectedSongId(null)} />}
-      <h2>Check out your song of the day:&nbsp;
-        <Link onClick={() => setSelectedSongId(songOfTheDay.song_id)}>{songOfTheDay.title}</Link>
-      </h2>
-      <Divider />
-      <h2>Top Songs</h2>
-      <LazyTable route={`http://${config.server_host}:${config.server_port}/top_songs`} columns={songColumns} />
-      <Divider />
-      {/* TODO (TASK 16): add a h2 heading, LazyTable, and divider for top albums. Set the LazyTable's props for defaultPageSize to 5 and rowsPerPageOptions to [5, 10] */}
-      <h2>Top Albums</h2>
-      <LazyTable route={`http://${config.server_host}:${config.server_port}/top_albums`} columns={albumColumns} defaultPageSize={5} rowsPerPageOptions={[5, 10]}/>
-      <Divider />
-      {/* TODO (TASK 17): add a paragraph (<p>text</p>) that displays the value of your author state variable from TASK 13 */}
-      <p>{author}</p>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+        <Dropdown className='Dropdown' placeholder='Select a districting' options={districtings} onChange={(value) => {
+          setRedistrictingId(value.value);
+          setSummaryText(``);
+        }} value={redistricting_id}/>
+  
+        <Dropdown className='Dropdown' placeholder='Select a Year' options={[2016, 2018, 2020]} onChange={(value) => {
+          setYear(value.value);
+          // setSummaryText(``);
+        }} value={year}/>
+  
+        <Dropdown className='Dropdown' placeholder='Select an Election Type' options={['house', 'presidential']} onChange={(value) => {
+          setElectionType(value.value);
+          // setSummaryText(``);
+        }} value={election_type}/>
+  
+        <Dropdown className='Dropdown' placeholder='Select a state' options={states} onChange={(value) => {
+          console.log(value)
+          setState(value.value);
+          setDistrict(null);
+          // setSummaryText(``);
+        }} value={state}/>
+  
+        {(state !== null && state !== 'All') && (
+          <Dropdown className='Dropdown' placeholder='Select a district' options={districts} onChange={(value) => {
+            setDistrict(value.value);
+            // setSummaryText(``);
+          }} value={district}/>
+        )}
+      </div>
+  
+      {(year !== null && redistricting_id !== null && election_type !== null) && (<h2>Election Results for {year} {election_type.charAt(0).toUpperCase() + election_type.slice(1)} Election with District Mapping '{redistricting_id}'</h2>)
+      }
+  
+      <p dangerouslySetInnerHTML={{__html: summary_text}}></p>
+  
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          autoHeight
+        />
+      </div>
+  
     </Container>
   );
-};
+      }
+//   return (
+//     <Container>
+
+//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a districting' options={districtings} onChange={(value) => {
+//     setRedistrictingId(value.value);
+//     setSummaryText(``);
+//   }} value={redistricting_id}/>
+
+
+//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a Year' options={[2016, 2018, 2020]} onChange={(value) => {
+//     setYear(value.value);
+//     // setSummaryText(``);
+//   }} value={year}/>
+
+
+//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select an Election Type' options={['house', 'presidential']} onChange={(value) => {
+//     setElectionType(value.value);
+//     // setSummaryText(``);
+//   }} value={election_type}/>
+
+
+//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a state' options={states} onChange={(value) => {
+//     console.log(value)
+//     setState(value.value);
+//     setDistrict(null);
+//     // setSummaryText(``);
+//   }} value={state}/>
+
+// {(state !== null && state !== 'All') && (
+//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a district' options={districts} onChange={(value) => {
+//     setDistrict(value.value);
+//     // setSummaryText(``);
+//   }} value={district}/>
+// )
+//       }
+//       {(year !== null && redistricting_id !== null && election_type !== null) && (<h2>Election Results for {year} {election_type.charAt(0).toUpperCase() + election_type.slice(1)} Election with District Mapping '{redistricting_id}'</h2>)
+//       }
+//       <p dangerouslySetInnerHTML={{__html: summary_text}}></p>
+//       <div>
+//         <DataGrid
+//           rows={data}
+//           columns={columns}
+//           autoHeight
+//         />
+//       </div>
+      
+//     </Container>
+//   )
+// }
