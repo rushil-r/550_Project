58667530971c636a38cdc65592249ed09d121530
anvitrao <76935670+anvitrao@users.noreply.github.com>
2023-05-02 23:28:14 -0400

    Updating with comments

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -20,30 +20,24 @@ export default function HomePage() {
   const [year, setYear]  = useState(queryParams.get('year')) 
   const [redistricting_id, setRedistrictingId] = useState(queryParams.get('redistricting')) 
   const [election_type, setElectionType] = useState(queryParams.get('election_type')) 
-
-  // const [year, setYear] = useState(2016);   
-  // const [redistricting_id, setRedistrictingId] = useState('Default'); 
-  // const [election_type, setElectionType] = useState('presidential'); 
   const [data, setData] = useState([]);
   const [summary, setSummary] = useState([]); 
   const [summary_text, setSummaryText]= useState(``); 
-  const [result, setResult] = useState('')
-  console.log(`State: ${state}`) 
-  console.log(`District: ${district}`) 
-  console.log(`Election Type: ${election_type}`)
    
-
+  //Get the list of all states in the dataset
   useEffect(() => {
     fetch(`http://${config.server_host}:${config.server_port}/get_states`)
       .then(res => {return res.json()})
       .then(resJson => { 
         const states = resJson.map((outcome) => (outcome.state));
+        // We include all to haev this as an option for our dropdown below
         states.push('All')
         setStates(states);
       })
       .catch(err => console.log(err));
   }, []);
 
+  //Get a list of all available districtings
   useEffect(() => {
     fetch(`http://${config.server_host}:${config.server_port}/get_districtings`)
     .then(res => {return res.json()})
@@ -52,8 +46,9 @@ export default function HomePage() {
       setDistrictings(districtings);
     })
     .catch(err => console.log(err));
-  }, [state, year, election_type]);
+  }, [state]);
 
+  //Get a list of the districts that a state can have
   useEffect(() => {
     if (state !== 'All' && state !== null) {
       fetch(`http://${config.server_host}:${config.server_port}/get_districts/?state=${state}`)
@@ -63,7 +58,6 @@ export default function HomePage() {
         const array = Array.from({ length: districts }, (_, index) => index + 1)
         array.push('All')
         setDistricts(array);
-        
       })
       .catch(err => console.log(err));
     } else {
@@ -71,7 +65,7 @@ export default function HomePage() {
     }
   }, [state]);
 
-  
+  //This is the main data fetching useEffect. Based on the states, we update the url that we are requesting data from. 
   useEffect(() => {
     let url = `http://${config.server_host}:${config.server_port}/?redistricting_id=${redistricting_id}&election_type=${election_type}&year=${year}`
     if (state !== 'All' && state !== null) {
@@ -94,6 +88,8 @@ export default function HomePage() {
   }, [redistricting_id, election_type, year,
   state, district]);
 
+  //This useEffect updates the 'summary' text that is displayed at the top of the home page. We have to use conditionals
+  //Because the format of the statement and input data is different depending on the selection params 
   useEffect(() => {
     if ((district != 'All' && district != null) || ((election_type == 'presidential') && (state != 'All') && (state != null))) {
       let tempSummaryText = ``;
@@ -150,25 +146,21 @@ export default function HomePage() {
   
         <Dropdown className='Dropdown' placeholder='Select a Year' options={[2016, 2018, 2020]} onChange={(value) => {
           setYear(value.value);
-          // setSummaryText(``);
         }} value={year}/>
   
         <Dropdown className='Dropdown' placeholder='Select an Election Type' options={['house', 'presidential']} onChange={(value) => {
           setElectionType(value.value);
-          // setSummaryText(``);
         }} value={election_type}/>
   
         <Dropdown className='Dropdown' placeholder='Select a state' options={states} onChange={(value) => {
           console.log(value)
           setState(value.value);
           setDistrict(null);
-          // setSummaryText(``);
         }} value={state}/>
   
         {(state !== null && state !== 'All') && (
           <Dropdown className='Dropdown' placeholder='Select a district' options={districts} onChange={(value) => {
             setDistrict(value.value);
-            // setSummaryText(``);
           }} value={district}/>
         )}
       </div>
@@ -190,52 +182,3 @@ export default function HomePage() {
     </Container>
   );
       }
-//   return (
-//     <Container>
-
-//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a districting' options={districtings} onChange={(value) => {
-//     setRedistrictingId(value.value);
-//     setSummaryText(``);
-//   }} value={redistricting_id}/>
-
-
-//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a Year' options={[2016, 2018, 2020]} onChange={(value) => {
-//     setYear(value.value);
-//     // setSummaryText(``);
-//   }} value={year}/>
-
-
-//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select an Election Type' options={['house', 'presidential']} onChange={(value) => {
-//     setElectionType(value.value);
-//     // setSummaryText(``);
-//   }} value={election_type}/>
-
-
-//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a state' options={states} onChange={(value) => {
-//     console.log(value)
-//     setState(value.value);
-//     setDistrict(null);
-//     // setSummaryText(``);
-//   }} value={state}/>
-
-// {(state !== null && state !== 'All') && (
-//   <Dropdown className='Dropdown' style={{ display: 'inline-block', width: '25%' }} placeholder='Select a district' options={districts} onChange={(value) => {
-//     setDistrict(value.value);
-//     // setSummaryText(``);
-//   }} value={district}/>
-// )
-//       }
-//       {(year !== null && redistricting_id !== null && election_type !== null) && (<h2>Election Results for {year} {election_type.charAt(0).toUpperCase() + election_type.slice(1)} Election with District Mapping '{redistricting_id}'</h2>)
-//       }
-//       <p dangerouslySetInnerHTML={{__html: summary_text}}></p>
-//       <div>
-//         <DataGrid
-//           rows={data}
-//           columns={columns}
-//           autoHeight
-//         />
-//       </div>
-      
-//     </Container>
-//   )
-// }
